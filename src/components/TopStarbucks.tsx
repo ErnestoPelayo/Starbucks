7583b93f6c7bff29ebc282856dd4110e7b7246fe
ErnestoPelayo <ernesto.p@itmexicali.edu.mx>
2024-08-27 10:15:37 -0700

    topbar fixed

--- src/components/TopStarbucks.tsx
+++ src/components/TopStarbucks.tsx
@@ -2,8 +2,9 @@
 const TopStarbucks = () => {
   return (
     <div className="bg-primary h-12">
-    <div className="w-5/6 mx-auto flex items-center h-full p-4">
+    <div className="lg:w-5/6 mx-auto flex items-center h-full p-4 justify-between">
         <p className="text-white font-custom2 sm:text-lg">STARBUKS® REWARDS</p>
+        <button className="lg:hidden border border-white rounded-full px-4 py-1 text-white text-sm">Join in the app</button>
     </div>
     </div>
   )
--- src/views/Rewards.tsx
+++ src/views/Rewards.tsx
@@ -1,17 +1,31 @@
 const Rewards = () => {
-    return (
-      <div className="bg-lightGreen min-h-screen relative">
-        <img className="absolute" draggable={false} />
-        <div className="relative h-full flex justify-center items-center w-5/6 mx-auto">
-          <div className="w-5/6 mt-10 flex flex-col justify-center items-center md:items-start">
-            <p className="text-2xl font-semibold text-center md:text-left md:text-lg">
-              FREE COFFEE IS A TAP AWAY
-            </p>
-          </div>
+  return (
+    <div className="bg-lightGreen min-h-screen relative">
+      <img className="absolute" draggable={false} />
+      <div className="relative h-full flex justify-center items-center lg:w-5/6 mx-auto">
+        <div className="w-5/6 mt-10 flex flex-col justify-center items-center md:items-start">
+          <p className="text-4xl font-semibold text-center md:text-left md:text-2xl" >
+            FREE COFFEE IS A TAP AWAY
+          </p>
+          <p className="mt-5 text-gray-600 text-center md:text-left text-xl ">
+            Join now to start earning Rewards.
+          </p>
+          <button className="mt-10 px-4 py-1 rounded-full bg-secondary text-white text-lg lg:hidden">
+            Join in the app
+          </button>
+          <button className="mt-10 px-4 py-1 rounded-full bg-secondary text-white text-lg hidden lg:block">
+            Join now
+          </button>
+          <p className="hidden lg:block mt-5 text-gray-600 text-center md:text-left text-xl">
+            Or <span className="underline hover:no-underline ">join in the app</span> for the best experience
+          </p>
+          <p className="lg:hidden mt-5 text-gray-600 text-center md:text-left text-xl">
+            <span className="underline hover:no-underline ">Or join in the app</span> 
+          </p>
         </div>
       </div>
-    );
-  };
-  
-  export default Rewards;
-  
+    </div>
+  );
+};
+
+export default Rewards;
